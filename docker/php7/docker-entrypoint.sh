075c8cc5d805f8718b1491b5e5fff239458c6887
Fabian Peter <fabian@peter.saarland>
2020-05-07 08:15:47 +0200

    fix: Images

--- docker/php7/docker-entrypoint.sh
+++ docker/php7/docker-entrypoint.sh
@@ -6,17 +6,18 @@ if [ "${1#-}" != "$1" ]; then
 	set -- php "$@"
 fi
 
-# Get Traefik IP (if existing)
-TRAEFIK_IP=`dig +short $TRAEFIK_HOST | head -n 1`
-
-echo $TRAEFIK_IP
-if [ "$TRAEFIK_IP" != "" ]
+if ["$TRAEFIK_ENABLED" != ""];
 then
-  export TRAEFIK_IP=$TRAEFIK_IP
+  # Get Traefik IP (if existing)
+  TRAEFIK_IP=`dig +short $TRAEFIK_HOST | head -n 1`
   
-  sed -i 's/172.16.238.10/"$TRAEFIK_IP"/'
-  sed -i 's/"httpcache" => array("enabled" => true),/"httpcache" => array("enabled" => false),/'
-  
+  if [ "$TRAEFIK_IP" != "" ]
+  then
+    export TRAEFIK_IP=$TRAEFIK_IP
+    
+    sed -i 's/172.16.238.10/"$TRAEFIK_IP"/' /var/www/html/config_production.php
+    sed -i 's/"httpcache" => array("enabled" => true),/"httpcache" => array("enabled" => false),/' /var/www/html/config_production.php
+    
+  fi
 fi
-
 exec "$@"
